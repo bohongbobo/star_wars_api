1a182b5b76b7c315a5af286b7cc6580294c25187
Bohong <bobo@Bohongs-MacBook.local>
2020-01-21 18:39:39 -0600

    Updates

---
+++ precache-manifest.ff71ad4b9ed6eab768b8ff698fe97f10.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53021ac0ebba89ae35cfca6f1d5ca366",
+    "url": "/Star_Wars_API/index.html"
+  },
+  {
+    "revision": "f0da0be10fc5432c2f8e",
+    "url": "/Star_Wars_API/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "553775f6a01b5ecb40ac",
+    "url": "/Star_Wars_API/static/js/2.1b9dd2f1.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Star_Wars_API/static/js/2.1b9dd2f1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f0da0be10fc5432c2f8e",
+    "url": "/Star_Wars_API/static/js/main.1bd2364e.chunk.js"
+  },
+  {
+    "revision": "bbc3b352e578c2a78952",
+    "url": "/Star_Wars_API/static/js/runtime-main.51273c55.js"
+  }
+]);
